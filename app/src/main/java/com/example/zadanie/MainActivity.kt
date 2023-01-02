7bbcba00458ab9ca1574ec1363235a853436240b
sztoslol <kubuszeski05@wp.pl>
2023-01-02 14:26:35 +0100

    Porownaie dziala

--- app/src/main/java/com/example/zadanie/MainActivity.kt
+++ app/src/main/java/com/example/zadanie/MainActivity.kt
@@ -2,13 +2,29 @@ package com.example.zadanie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            var haslo = findViewById<TextInputEditText>(R.id.Haslo).text.toString()
+            var potwierdz = findViewById<TextInputEditText>(R.id.Potwierdz).text.toString()
+
+            if( haslo==potwierdz){
+                findViewById<TextView>(R.id.textView).text="Hasla sa zgodne"
+            }
+            else{
+                findViewById<TextView>(R.id.textView).text="Hasla nie sa zgodne"
+            }
+
+        }
     }
-    //Marcin Kummer
+
 
 
 
